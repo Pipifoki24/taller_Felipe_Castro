5139b3623df30457341bd06e2faa4d9ffed1d178
Pipifoki24 <135989592+Pipifoki24@users.noreply.github.com>
2023-06-15 14:52:58 +0000

    implementacion de modulos

---
+++ programa.py
@@ -0,0 +1,9 @@
+from matematicas import sumar, restar, multiplicar, dividir
+from algebra import distancia
+
+print(sumar(2, 3))
+print(restar (4, 2))
+print(multiplicar(10, 4))
+print(dividir(36, 9))
+print(distancia(1,2,3,4))
+print(distancia(4,3,3,6))
